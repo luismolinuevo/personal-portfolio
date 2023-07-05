14d9bb5546e5339156fefab3d7cf9f54712e63b3
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-07-05 04:39:26 -0400

    footer complete

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import './App.css'
-import Homepage from './pages/Homepage'
-import Navbar from './components/Navbar'
-import AboutMe from './pages/AboutMe'
-import Skills from './pages/Skills'
-import Projects from './pages/Projects'
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
+import AboutMe from "./pages/AboutMe";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className='min-h-screen bg-[#e5e5e5] text-[black] font-tit m-0 p-0 box-border'>
-       <Navbar/>
-       <Homepage/>
-       <AboutMe/>
-       <Skills/>
-      <Projects/>
+    <div className="min-h-screen bg-[#e5e5e5] text-[black] font-tit m-0 p-0 box-border">
+      <Navbar />
+      <Homepage />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-project/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BsGithub, BsLinkedin, BsFillFileEarmarkFill, BsYoutube } from "react-icons/bs";
+import Resume from "../assets/Resume.pdf";
+
+export default function Footer() {
+  return (
+    <footer className='bg-main w-full text-white'>
+        <div className='p-4'>
+            <div className='flex gap-2 justify-center'>
+            <a
+              href="https://github.com/luismolinuevo"
+              target="_blank"
+              className="cursor-pointer text-[30px]"
+            >
+              <BsGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/luismolinuevo/"
+              target="_blank"
+              className="cursor-pointer text-[30px]"
+            >
+              <BsLinkedin />
+            </a>
+            <a
+              href="https://www.youtube.com/@luiscodes5533"
+              target="_blank"
+              className="cursor-pointer text-[30px]"
+            >
+              <BsYoutube/>
+            </a>
+            <a href={Resume} target="blank_" className="cursor-pointer  text-[30px]">
+              <BsFillFileEarmarkFill />
+            </a> 
+            </div>
+            <p className='text-center  text-[20px]'>@ Luis Molinuevo 2023</p>
+        </div>
+    </footer>
+  )
+}
