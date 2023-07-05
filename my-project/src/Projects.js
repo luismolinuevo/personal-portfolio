d6ea9ade392be29b407df22306eebe281fe8bd2b
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-07-05 03:21:31 -0400

    created array of projects, project file

---
+++ my-project/src/Projects.js
@@ -0,0 +1,42 @@
+import TechNiche from './assets/techniche.png';
+import WorkoutApp from "./assets/WorkoutGif.gif";
+import JournalApp from "./assets/JournalGif.gif";
+
+const projects = [
+    {
+       id: 1,
+       name: "Tech Niche",
+       about: "This is a Full Stack app created with React and Express. Its a ecommerce app for tech with bids, bartering via chat, and negoitation via chat. It allows users to create, delete, and edit there post.",
+       gitHub: "https://github.com/luismolinuevo/digitaldish",
+       live: "https://digitaldish.vercel.app/",
+       techStack: ["React.js", "Tailwind", "Redux", "Express", "Prisma", "MySQL", "Cloudinary", "Socket.io"],
+       img: TechNiche
+    },
+    {
+        id: 2,
+        name: "Shopping Cart",
+        about: "This app was created with React.js, TailwindCSS, and Redux Tool kit. This app allows user to add and remove items from a shopping cart. Users could also add or decrease the amount of items in a cart. You could also fully clear the cart.",
+        gitHub: "https://github.com/luismolinuevo/shoppingcart",
+        live: "https://shoppingcart-nu-liart.vercel.app/",
+        techStack: ["React.js", "TailwindCSS", "Redux Tool Kit"],
+        img: WorkoutApp
+    },
+    {
+        id: 3,
+        name: "Fitszy",
+        about: "This app allows users to create a personal account, custom workouts, and custom exercises for specific workouts. It saves user information and allows users to delete, create, add, and edit their workouts and exercises.",
+        gitHub: "https://github.com/luismolinuevo/workoutappv2",
+        live: null,
+        techStack: ["React.js", "CSS", "Express", "SequelizeORM", "PostgreSQL", ],
+        img: JournalApp
+    },
+    {
+
+    },
+    {
+        
+    }
+]
+
+export default projects;
+
--- my-project/src/pages/Projects.jsx
+++ my-project/src/pages/Projects.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import ImageCard from "../components/ImageCard";
+import projects from "../Projects";
 
 export default function Projects() {
   return (
-    <div>
-        
-    </div>
-  )
+    <section className="px-[50px] py-6 md:px-[80px]">
+      <div>
+      <h1 className="flex justify-center text-main text-modileHeading md:text-heading font-bold py-6">
+          Projects
+        </h1>
+        {
+          projects && projects.length != 0 ? (
+            projects.map((project) => (
+              <div>
+                <ImageCard about={project.about} title={project.name} id={project.id} gitHub={project.gitHub} liveLink={project.live}/>
+              </div>
+            ))
+          ) : <p></p>
+        }
+      </div>
+    </section>
+  );
 }
