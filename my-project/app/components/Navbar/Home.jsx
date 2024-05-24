1d96b3c44a05a876729acc8241853e4b183c2483
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-23 23:46:39 -0400

    inital home page

---
+++ my-project/app/components/Navbar/Home.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { BsGithub, BsLinkedin, BsFillFileEarmarkFill } from "react-icons/bs";
+import Resume from "../assets/Resume.pdf";
+import RainCloud from "../components/Cloud";
+
+export default function Homepage() {
+  return (
+    <section
+      className="w-full h-screen px-[50px] md:px-[80px] md:flex justify-center block"
+      id="home"
+    >
+      <div>
+        <main className="w-full flex justify-center text-center pt-[40px] md:pt-[125px]">
+          <div className="flex flex-col">
+            <motion.h1
+              className="text-[40px] text-main"
+              animate={{ y: 0 }}
+              initial={{ y: -100 }}
+              transition={{ duration: 0.7, delay: 0.2 }}
+            >
+              Hello!
+            </motion.h1>
+            <motion.h2
+              className="text-[65px]"
+              animate={{ y: 0 }}
+              initial={{ y: -100 }}
+              transition={{ duration: 0.7, delay: 0.4 }}
+            >
+              Im Luis
+            </motion.h2>
+            <motion.h3
+              className="text-[65px]"
+              animate={{ y: 0 }}
+              initial={{ y: -100 }}
+              transition={{ duration: 0.7, delay: 0.6 }}
+            >
+              <span className="text-main">Moli</span>nuevo
+            </motion.h3>
+            <motion.h4
+              className="text-[35px]"
+              animate={{ y: 0 }}
+              initial={{ y: -100 }}
+              transition={{ duration: 0.7, delay: 0.8 }}
+            >
+              Full Stack Web Developer
+            </motion.h4>
+          </div>
+        </main>
+        <aside className="hidden md:flex flex-col absolute top-[350px] right-12">
+          <motion.div
+            className="flex flex-col items-center justify-center"
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.2 }}
+          >
+            <h1 className="mb-2 transform rotate-90 text-[20px]">Find me at</h1>
+            <hr className="w-[2px] h-8 bg-black mt-6 mb-1" />
+            <div className="flex flex-col items-center justify-center text-main text-[25px] gap-2">
+              <a
+                href="https://github.com/luismolinuevo"
+                target="_blank"
+                className="cursor-pointer"
+                title="github"
+              >
+                <BsGithub />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/luismolinuevo/"
+                target="_blank"
+                className="cursor-pointer"
+                title="linkedin"
+              >
+                <BsLinkedin />
+              </a>
+              <a
+                href={Resume}
+                target="blank_"
+                className="cursor-pointer"
+                title="Resume"
+              >
+                <BsFillFileEarmarkFill />
+              </a>
+            </div>
+          </motion.div>
+        </aside>
+        <div className="flex justify-center gap-2 mt-4">
+          <motion.a
+            href="#projects"
+            className="bg-main text-white p-2 md:p-3 text-xl cursor-pointer"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.4 }}
+          >
+            My work
+          </motion.a>
+          <motion.a
+            href="#contactme"
+            className="border-2 border-main p-2 md:p-3 text-main text-xl cursor-pointer"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.6 }}
+          >
+            Hire Me!
+          </motion.a>
+        </div>
+        <div className="flex justify-center pt-2">
+          <motion.a
+            href={Resume}
+            target="blank_"
+            title="Resume"
+            className="border-2 border-main p-2 md:p-3 text-main text-xl cursor-pointer"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.8 }}
+          >
+            Resume
+          </motion.a>
+        </div>
+      </div>
+      {/* <RainCloud/> */}
+    </section>
+  );
+}
--- my-project/app/page.js
+++ my-project/app/page.js
@@ -1,5 +1,9 @@
+import Homepage from "./components/Navbar/Home";
+
 export default function Home() {
   return (
-   <></>
+    <div className="min-h-screen bg-[#e5e5e5] text-[black] font-tit m-0 p-0 box-border">
+      <Home />
+    </div>
   );
 }
