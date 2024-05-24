6e2231a4d928ae34fd35a8ec49559d35d6134512
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-24 02:53:27 -0400

    added missing skills

--- my-projectnew/app/page.js
+++ my-projectnew/app/page.js
@@ -1,11 +1,13 @@
 import AboutMe from "./components/Aboutme";
 import Homepage from "./components/Home";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
     <div>
       <Homepage />
       <AboutMe />
+      <Skills />
     </div>
   );
 }
