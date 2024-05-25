9ab4669aef0a776a8be3af83ab9e6a6c2d5ca963
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-25 03:47:06 -0400

    added contact me, updated success message to be temopory

--- my-projectnew/app/page.js
+++ my-projectnew/app/page.js
@@ -2,6 +2,7 @@ import AboutMe from "./components/PageSections/Aboutme";
 import Homepage from "./components/PageSections/Home";
 import Skills from "./components/PageSections/Skills";
 import Projects from "./components/PageSections/Projects";
+import ContactMe from "./components/PageSections/Contactme";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutMe />
       <Skills />
       <Projects />
+      <ContactMe />
     </div>
   );
 }
