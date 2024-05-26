0ec91776be1a8215bdd1c7ad5504239968252955
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-05-26 00:38:37 -0400

    added bloomin

--- my-projectnew/app/lib/Projects.js
+++ my-projectnew/app/lib/Projects.js
@@ -1,41 +1,57 @@
 const projects = [
-    {
-       id: 1,
-       name: "Tech Niche",
-       about: "This is a Full Stack app created with React and Express. Its a ecommerce app for tech with bids, bartering via chat, and negoitation via chat. It allows users to create, delete, and edit there post.",
-       gitHub: "https://github.com/luismolinuevo/digitaldish",
-       live: "https://digitaldish.vercel.app/",
-       techStack: "React.js, Tailwind, Redux, Express, Prisma, MySQL, Cloudinary, Socket.io",
-       img: "/techniche.png",
-       status: 'Completed'
-    },
-    {
-        id: 2,
-        name: "Shopping Cart",
-        about: "This app was created with React.js, TailwindCSS, and Redux Tool kit. This app allows user to add and remove items from a shopping cart. Users could also add or decrease the amount of items in a cart. You could also fully clear the cart.",
-        gitHub: "https://github.com/luismolinuevo/shoppingcart",
-        live: "https://shoppingcart-nu-liart.vercel.app/",
-        techStack: "React.js, TailwindCSS, Redux Tool Kit",
-        img: "/ShoppingCart.png",
-        status: 'Completed'
-    },
-    {
-        id: 3,
-        name: "Fitszy",
-        about: "This app allows users to create a personal account, custom workouts, and custom exercises for specific workouts. It saves user information and allows users to delete, create, add, and edit their workouts and exercises.",
-        gitHub: "https://github.com/luismolinuevo/workoutappv2",
-        live: null,
-        techStack: "React.js, CSS, Express, SequelizeORM, PostgreSQL",
-        img: "/WorkoutGif.gif",
-        status: 'Completed'
-    },
-    // {
+  {
+    id: 1,
+    name: "Bloomin",
+    about:
+      "Created a social media app for eco-friendly tips with REST API, dynamic frontend, authentication, image upload, OpenAI integration, Google OAuth, and Jest testing, showcasing full-stack development skills.",
+    gitHub: "https://github.com/luismolinuevo/Bloomin",
+    live: "https://bloomin-snowy.vercel.app/",
+    techStack:
+      "Next.js, TailwindCSS, Redux,  Material Tailwind, Express, Prisma, Cloudinary, Jest",
+    img: "/Bloomin.png",
+    status: "Completed",
+  },
+//   {},
+  {
+    id: 1,
+    name: "Tech Niche",
+    about:
+      "This is a Full Stack app created with React and Express. Its a ecommerce app for tech with bids, bartering via chat, and negoitation via chat. It allows users to create, delete, and edit there post.",
+    gitHub: "https://github.com/luismolinuevo/digitaldish",
+    live: "https://digitaldish.vercel.app/",
+    techStack:
+      "React.js, TailwindCSS, Redux, Express, Prisma, MySQL, Cloudinary, Socket.io",
+    img: "/techniche.png",
+    status: "Completed",
+  },
+  {
+    id: 2,
+    name: "Shopping Cart",
+    about:
+      "This app was created with React.js, TailwindCSS, and Redux Tool kit. This app allows user to add and remove items from a shopping cart. Users could also add or decrease the amount of items in a cart. You could also fully clear the cart.",
+    gitHub: "https://github.com/luismolinuevo/shoppingcart",
+    live: "https://shoppingcart-nu-liart.vercel.app/",
+    techStack: "React.js, TailwindCSS, Redux Tool Kit",
+    img: "/ShoppingCart.png",
+    status: "Completed",
+  },
+  {
+    id: 3,
+    name: "Fitszy",
+    about:
+      "This app allows users to create a personal account, custom workouts, and custom exercises for specific workouts. It saves user information and allows users to delete, create, add, and edit their workouts and exercises.",
+    gitHub: "https://github.com/luismolinuevo/workoutappv2",
+    live: null,
+    techStack: "React.js, CSS, Express, SequelizeORM, PostgreSQL",
+    img: "/WorkoutGif.gif",
+    status: "Completed",
+  },
+  // {
 
-    // },
-    // {
-        
-    // }
-]
+  // },
+  // {
+
+  // }
+];
 
 export default projects;
-
