389939504e6d59cf3e2bd3f9e9a923b9c5772ffc
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-07-22 22:19:21 -0400

    created sitemap

---
+++ my-project/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://luismolinuevo",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://luismolinuevo/project",
+      lastModified: new Date(),
+    },
+  ];
+}
